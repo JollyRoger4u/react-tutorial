fa12c9691f63c3794f66604c3045ecb155746d23
JollyRoger4u <petterssonroger@hotmail.com>
2020-01-25 13:38:20 +0100

    dynamic greeting depending on tume of day

--- src/tutorial/test1.jsx
+++ src/tutorial/test1.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 function MyInfo() {
+
+    const firstName = "Roger";
+    const lastName = "Pettersson";
+    const date = new Date();
+    const time = date.getHours();
+    let greeting;
+    if (time < 12) {
+        greeting = "good morning"
+    } else if (time > 12 && time < 18) {
+        greeting = "good day"
+    } else {
+        greeting = "good evening"
+    }
     return (
         <div>
-            <h1>Hellu, me nam iz Roger</h1>
+            <p>{`${greeting}`}</p>
+            <h1>Hellu, me nam iz {`${firstName} ${lastName}`}</h1>
             <p>I am currently learning react</p><ol>
                 <p>I would like to visit</p>
                 <li>China</li>
