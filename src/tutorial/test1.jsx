1143429c11770691d89baaca849c0c8ac21dd824
JollyRoger4u <petterssonroger@hotmail.com>
2020-01-25 13:17:38 +0100

    first assignment done

---
+++ src/tutorial/test1.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Hellu, me nam iz Roger</h1>
+            <p>I am currently learning react</p><ol>
+                <p>I would like to visit</p>
+                <li>China</li>
+                <li>Japan</li>
+                <li>the US of A</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MyInfo;
